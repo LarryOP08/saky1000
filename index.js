bac21a4410ef2923c674f891a0481894d2a0ce38
LarryOP08 <98734115+LarryOP08@users.noreply.github.com>
2022-03-23 19:58:07 +0100

    k, aggiustato

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client({
     intents: ["GUILD_VOICE_STATES", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILDS"]
 })
 
-client.login(process.emv.Larry)
+client.login(process.emv.Token)
 
 client.on("ready", () => {
     console.log("BotONLINE")
